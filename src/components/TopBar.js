50198baaa4ffd3ef2c924b4dda84f38f669ab544
Ricardo Montenegro <ramn21@gmail.com>
2021-08-13 16:44:54 +0200

    Add topbar component

---
+++ src/components/TopBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import LogOut from './LogOut';
+import TopBarStyle from '../styles/TopBar.module.css'
+
+const TopBar = ({userName}) => (
+
+  <div className={TopBarStyle.topBar}>
+    <h2 className={TopBarStyle.text}>
+      Welcome to your WallyWallet
+    </h2>
+    <div className={TopBarStyle.userBtnContainer}>
+      <h3 className={TopBarStyle.text}>{userName}</h3>
+      <LogOut />
+    </div>
+  </div>
+);
+
+export default TopBar;
