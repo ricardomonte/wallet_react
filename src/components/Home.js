da41378394d2f84b3fc894a229b269b0b1c22199
Ricardo Montenegro <ramn21@gmail.com>
2021-08-13 16:40:17 +0200

    Add home component

---
+++ src/components/Home.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import Dashboard from './Dashboard';
+import HomeStyle from '../styles/Home.module.css';
+
+// import { AccountProvider } from '../context/AccountContext';
+
+
+
+const Home = () => {
+  const [authenticated, setAuthenticated] = useState(false)
+
+  const readCookie = () => {
+    console.log(document.cookie.indexOf('user'))
+    console.log(Cookies.get('user'))
+    if(document.cookie.indexOf('user') > -1) {
+      setAuthenticated(true);
+    }
+  }
+  useEffect(() => {
+    readCookie()
+  }, [])
+
+  if(authenticated) {
+
+    return <Dashboard />
+  }
+  return(
+    <>
+    <div className={HomeStyle.container}>
+      <h1 className={HomeStyle.willwallet}>WilliWallet</h1>
+      <div className={HomeStyle.containerLink}>
+        <Link to="/sign_up" className={HomeStyle.link}>
+          Sign Up
+        </Link>
+      </div>
+      <div className={HomeStyle.containerLink}>
+        <Link to="/sign_in" className={HomeStyle.link}>
+          Sign In
+        </Link>
+      </div>
+    </div>
+  </>
+  )
+}
+
+export default Home
